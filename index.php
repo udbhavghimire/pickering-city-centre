a0bb04ef84c795ad6c0b77aff8cf22f1604ba569
Udbhav Ghimire <ghimireudbhav@gmail.com>
2023-07-27 22:07:11 +0545

    changesssss

--- index.php
+++ index.php
@@ -58,15 +58,24 @@
                 <a href="/" class="navbar-brand">
                 <img src="./images/the-pickering-logo.png" class="navbar-brand"  alt="The Pickeiring Logo">
                 </a>
-                <a href="#Contact" class="btn  call-btn d-block d-md-none  py-2 btn-lg">
-          Register Now
-        </a>
+               <div class ="d-flex">
+               <div class="dropdown d-block d-md-none pt-5">
+                            <a class="nav-link scrollto" href="#cities">Cities
+                            <i class="bi bi-caret-down-fill"></i>
+                            </a>
+                                <div class="dropdown-menu" aria-labelledby="hoverDropdown">
+                                    <a class="dropdown-item" href="../pre-construction-homes-pickering/">Pre construction homes  Pickering</a>
+                                    <a class="dropdown-item" href="../pre-construction-homes-whitby/">Pre construction homes  Whitby</a>
+                                    <a class="dropdown-item" href="../pre-construction-homes-ajax/">Pre construction homes  Ajax</a>
+                                    <a class="dropdown-item" href="../pre-construction-homes-oshawa/">Pre construction homes  Oshawa</a>
+                                </div>
+               </div>
                 <button class="d-block d-md-none btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 12H18V10H0V12ZM0 7H18V5H0V7ZM0 0V2H18V0H0Z" fill="black" />
           </svg>
         </button>
-                <div class="row me-0 row-cols-2 collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="row me-0 row-cols-2 collapse navbar-collapse pt-5" id="navbarSupportedContent">
                     <div class="col-12 d-flex justify-content-start justify-content-md-end">
                         <ul class="navbar-nav mb-2 mb-lg-0 align-items-start align-items-md-center">
                             <li class="nav-item">
