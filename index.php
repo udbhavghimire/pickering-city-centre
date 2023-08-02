7e5ef584cdcb8d2a907a5b667065d2a22bad00c7
Udbhav Ghimire <ghimireudbhav@gmail.com>
2023-08-02 14:22:01 +0545

    sitemap added

--- index.php
+++ index.php
@@ -28,6 +28,8 @@
     <meta property="og:image" content="https://thepickering.ca/Pickering-City-Centre-Condos-Coming-Soon--1-v9-full.jpg"> 
     <meta property="og:site_name" content="Pickering City Centre" />
 
+    <link rel="sitemap" type="application/xml" title="Sitemap" href="./sitemap.xml" />
+
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
