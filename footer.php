f0e176b4314aa877f9c1210b33181880c1677579
Udbhav Ghimire <ghimireudbhav@gmail.com>
2023-08-02 13:50:30 +0545

    foloating button

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
+
+<div class="floating fixxcont2 mb-3">
+        <a href="#Contact" class="btn bgggggggg br-m d-block d-md-none w-100 py-2 shadow-lg">Send a message  
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-dots" viewBox="0 0 16 16">
+                <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path><path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z">
+                </path>
+            </svg>
+            </a>
+        </div>
+
 <div class="mt-5 pt-3 ">
         <img loading="lazy " src="../images/line.png " alt="Line image for character " class="img-fluid foot-up-img " />
     </div>
